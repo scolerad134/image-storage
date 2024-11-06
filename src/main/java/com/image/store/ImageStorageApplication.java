8d7b70072a3644c119a3dc140f757df9c917e817
scolerad134 <gugac134@gmail.com>
2024-11-06 17:17:11 +0300

    init project and init core and mail microservices

---
+++ src/main/java/com/image/store/ImageStorageApplication.java
@@ -0,0 +1,13 @@
+package com.image.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageStorageApplication.class, args);
+	}
+
+}
